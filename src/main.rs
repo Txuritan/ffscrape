84fe143a19c35d8684fe274142d3763f0be4e888
Txuritan <txurtian@yahoo.com>
2018-01-30 14:06:58 -0500

    Almost got title selection working

--- src/main.rs
+++ src/main.rs
@@ -22,17 +22,33 @@ error_chain! {
 
 mod sites;
 
-fn download(matches: clap::ArgMatches, url: &str, site: &sites::Sites) -> Result<()> {
-
-    let client = reqwest::Client::new();
-
-    let mut res = client.get(url);
+fn setup_headers(matches: &clap::ArgMatches) -> reqwest::header::Headers {
+    let mut headers = reqwest::header::Headers::new();
 
     if !matches.is_present("disable") {
-        res.header(reqwest::header::UserAgent::new(format!("FFScrape/{:?} reqwest/{:?}", crate_version!(), "0.8.4")));
+        headers.set(reqwest::header::UserAgent::new(format!("ffscrape/{:?} reqwest/{:?} hyper/{:?} rust/{:?}", crate_version!(), "0.8.4", "0.11.9", "1.25.0-nightly")));
     }
 
-    return Ok(())
+    return headers;
+}
+
+fn download(client: &reqwest::Client, matches: &clap::ArgMatches, url: &str, site: sites::Sites) {
+
+    let mut res = client.get(url)
+        .headers(setup_headers(matches))
+        .send().unwrap();
+
+    assert!(res.status().is_success());
+
+    let body = res.text().unwrap();
+
+    let document = scraper::Html::parse_document(&body);
+
+    match site {
+        sites::Sites::FanFictionNet => {
+            let info = sites::fanfiction::get_info(document, url);
+        }
+    }
 }
 
 fn run() -> Result<()> {
@@ -47,7 +63,7 @@ fn run() -> Result<()> {
                 message
             ))
         })
-        .level(log::LevelFilter::Debug)
+        .level(log::LevelFilter::Info)
         .chain(std::io::stdout())
         .apply().expect("Error: Fern unable to create logger");
 
@@ -76,6 +92,8 @@ fn run() -> Result<()> {
              .help("Disables Reqwest useragent"))
         .get_matches();
 
+    let client = reqwest::Client::new();
+
     let urls = app.values_of("url").map(|vals| vals.collect::<Vec<_>>());
 
     info!("URLs: {:?}", &urls.clone().unwrap_or_else(Vec::new));
@@ -86,7 +104,7 @@ fn run() -> Result<()> {
         if regex_fanfiction_net.is_match(&url) {
             info!("URL: {:?}, matches Fanfiction.Net Regex", url);
 
-            download(app, url, sites::Sites::FanFictionNet);
+            download(&client, &app, &url, sites::Sites::FanFictionNet);
         }
     }
 
--- src/sites/fanfiction.rs
+++ src/sites/fanfiction.rs
@@ -1,22 +1,54 @@
 use std::string;
-use std::vec;
 
 use chrono;
+use scraper;
 use vec_map;
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct  Chapter {
+    content: string::String,
+}
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct FanFiction {
     pub title: string::String,
     pub author: string::String,
-    pub fandoms: vec::Vec<string::String>,
+    pub fandoms: vec_map::VecMap<string::String>,
     pub rating: string::String,
     pub language: string::String,
-    pub genres: vec::Vec<string::String>,
-    pub characters: vec::Vec<string::String>,
+    pub genres: vec_map::VecMap<string::String>,
+    pub characters: vec_map::VecMap<string::String>,
     pub words: u32,
     pub favorites: u32,
     pub follows: u32,
     pub published: chrono::DateTime<chrono::offset::Utc>,
     pub updated: chrono::DateTime<chrono::offset::Utc>,
     pub chapters: vec_map::VecMap<string::String>,
+}
+
+pub fn get_info(document: scraper::Html, url: &str) -> (
+    string::String, // Title
+    // string::String, // Author
+    // vec_map::VecMap<string::String>, // Fandoms
+    // string::String, // Rating
+    // string::String, // Language
+    // vec_map::VecMap<string::String>, // Genres
+    // vec_map::VecMap<string::String>, // Characters
+    // u32, // Words
+    // u32, // Favorites
+    // u32, // Follows
+    // chrono::DateTime<chrono::offset::Utc>, // Published
+    // chrono::DateTime<chrono::offset::Utc>, // Updated
+) {
+    let selector_title = scraper::Selector::parse("title").unwrap();
+
+    let title = string::String::from("");
+
+    for element in document.select(&selector_title) {
+        info!("{:?}", element.text());
+    }
+
+    return (
+        title,
+    );
 }
